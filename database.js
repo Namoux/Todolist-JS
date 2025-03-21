4e13f23b14e7b95402369de24bfe69d2646a7e67
Namoux <quoc-nam.ngo@laplateforme.io>
2025-03-21 16:08:38 +0100

    Checkbox, delete, chevron a jour

--- database.js
+++ database.js
@@ -7,47 +7,162 @@ export function OnClickaddTask() {
 }
 
 // fonction setter
-export function newTask(title, description) {
-    const task = {
+export function newTask(ID, title, description, check) {
+    
+    let task = {
+        ID,
         title,
-        description
+        description,
+        check
     };
-    const tasks = JSON.parse(localStorage.getItem("tasks") ?? "[]");
+    
+    let tasks = JSON.parse(localStorage.getItem("tasks") ?? "[]");
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-
+    
     return task;
 }
 
 // fonction getter 
 export function getAllTasks(){
-    const tasks = localStorage.getItem("tasks");
+    let tasks = localStorage.getItem("tasks");
     if(tasks == null) return null;
-
-    const task = JSON.parse(tasks);
+    
+    let task = JSON.parse(tasks);
     // console.log(task);
 
     return task;
 }
 
+// Compteur Pour definir un ID a chaque tache
+let compteur = 0; 
+export function CreateId() {
+    let Alltasks = getAllTasks();
+    if (Alltasks != null) {
+        Alltasks.forEach((task) => {
+            Alltasks.forEach((task) => {
+                compteur = task.ID +1;
+            })
+        }); 
+    }
+    return compteur;
+
+}
+
 // fonction remove 
-export function removeTask(){
-    const tasks = localStorage.removeItem("tasks");
+export function removeAllTasks(){
+    let tasks = localStorage.removeItem("tasks");
 }
 
 // Affichage des taches sur le HTML
 export function cloneTask (task) {
     const scrollTask = document.querySelector(".scrollTask");
     const template = document.querySelector(".post-template");
-
+    
     const cloneTemplate = template.content.cloneNode(true);
-
+    
     const title = cloneTemplate.querySelector(".post-titre>p");
     title.textContent = task.title;
-
+    
     const description = cloneTemplate.querySelector(".post-description>p");
     description.textContent = task.description;
+    // console.log("id1 :", task.ID);
+    
 
+    // Boutont delete de chaque tache
+    const BTNdelete = cloneTemplate.querySelector("#delete");
+    const divTask = cloneTemplate.querySelector(".task");
+    const divDescritption = cloneTemplate.querySelector(".post-description");
+    
+    BTNdelete.addEventListener("click", () => {
+        let PositionTab = 0;
+        let Alltasks = getAllTasks();
+
+        // console.log("id2 :", task.ID);
+        for(let i = 0; i < Alltasks.length; i++) {
+            // console.log("TaskID ds le for :", Alltasks[i].ID);
+            if (Alltasks[i].ID === task.ID) {
+                console.log("i :", i);
+                PositionTab = i;
+                break;
+            }
+        }
+
+        console.log("PositionTab :", PositionTab);
+        Alltasks.splice(PositionTab, 1);
+        divTask.remove();
+        divDescritption.remove();
+        localStorage.setItem("tasks", JSON.stringify(Alltasks));
+        console.log("task removed");
+        console.log("task are", Alltasks);
+    });  
+    
+
+    // On lit la description en activant le chevron
+    const chevrons = cloneTemplate.querySelector("#chevron");
+    chevrons.addEventListener("click", () => {
+        const conteneur = divTask.nextElementSibling;
+        conteneur.classList.toggle("appear");
+        divTask.classList.toggle("appear");
+        chevrons.classList.toggle("fa-chevron-down");
+        chevrons.classList.toggle("fa-chevron-up");
+    });
+    
+    // On retourne la valeur si la tache est effectuée ou pas
+    const checkbox = cloneTemplate.querySelector("#mycheck");
+    let Alltasks = getAllTasks();
+    checkbox.addEventListener("click", () => {
+        let PositionTab = 0;
+        
+        console.log("id2 :", task.ID);
+        for(let i = 0; i < Alltasks.length; i++) {
+            // console.log("TaskID ds le for :", Alltasks[i].ID);
+            if (Alltasks[i].ID === task.ID) {
+                PositionTab = i;
+                console.log("PositionTab :", i);
+                break;
+            }
+        }
+        
+        if (Alltasks[PositionTab].check == false) {
+            console.log("false ok");
+            Alltasks[PositionTab].check = true;
+            console.log("taskcheck apres le click", task.check);
+            localStorage.setItem("tasks", JSON.stringify(Alltasks));	
+        } else if (Alltasks[PositionTab].check == true) {
+            console.log("true ok");
+            Alltasks[PositionTab].check = false;
+            localStorage.setItem("tasks", JSON.stringify(Alltasks));
+        } else {
+            console.log("Marche pas");
+        }
+        Alltasks = getAllTasks();
+        console.log("task are", Alltasks);
+    });
+
+    // Au chargement de la page, on verifie les checkboxs
+    let PositionTab = 0;
+        
+    // console.log("id2 :", task.ID);
+    for(let i = 0; i < Alltasks.length; i++) {
+        // console.log("TaskID ds le for :", Alltasks[i].ID);
+        if (Alltasks[i].ID === task.ID) {
+            console.log("i :", i);
+            PositionTab = i;
+            break;
+        }
+    }
+
+    if (Alltasks[PositionTab].check == true) {
+        checkbox.checked = true;
+        console.log("c'est true");
+    } else if (Alltasks[PositionTab].check == false) {
+        console.log("c'est false");
+        checkbox.checked = false;
+    } else {
+        console.log("Marche pas");
+    }
+    
     scrollTask.appendChild(cloneTemplate);
 };
 
@@ -56,13 +171,17 @@ export function onSubmit(event){
     event.preventDefault();
     const formData = new FormData(event.target);
     console.log(formData);
-    
-    const task = {
+
+    let task = {
+        ID: CreateId(), 
         title: formData.get("inputTitre"),
         description: formData.get("description"),
-    }
-    newTask(task.title, task.description);
+        check: false,
+    };
+
+    newTask(task.ID, task.title, task.description, task.check);
     OnClickaddTask();
     cloneTask(task);
-    
+    let Alltasks = getAllTasks();
+    console.log("task are", Alltasks);
 }   
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getAllTasks, newTask, removeTask, OnClickaddTask, onSubmit, cloneTask } from "./database.js";
+import { getAllTasks, newTask, removeAllTasks, OnClickaddTask, onSubmit, cloneTask, CreateId } from "./database.js";
 
 const form = document.querySelector("form");
 const addTask = document.querySelector(".addTask");
@@ -10,53 +10,16 @@ closeWriteTask.addEventListener("click", OnClickaddTask);
 
 // Appel de la fonction pr recuperer les taches présentes dans le local storage
 const Alltasks = getAllTasks();
-console.log("task are", Alltasks);
-Alltasks.forEach((task) => {
-    cloneTask(task);
-}); 
+
+console.log("tasks are", Alltasks);
+if (Alltasks != null) {
+    Alltasks.forEach((task, i) => {
+        cloneTask(task);
+    }); 
+}
 
 // Appel de la fonction pour Creation d'une tache dans le local storage
 form.addEventListener("submit", onSubmit);
-
-const deleteTasks = document.querySelectorAll("#delete");
-const divTask = document.querySelectorAll(".task");
-
-for (let i = 0; i < deleteTasks.length; i++) {
-    deleteTasks[i].addEventListener("click", function () {
-        console.log("delete");
-        Alltasks.splice(i,1);
-        console.log("task are", Alltasks);
-        localStorage.setItem("tasks", JSON.stringify(Alltasks));
-        
-        for( let j = 0; j < divTask.length; j++) {
-            divTask[i].remove();
-            console.log("template removed");
-        }
-        // location.reload();
-    });
-}
-
-// Affichage de la description au clic du chevron
-const chevrons = document.querySelectorAll("#chevron");
-
-for (let i = 0; i < chevrons.length; i++) {
-    console.log(chevrons[i]);
-    chevrons[i].addEventListener("click", function () {
-        console.log("clic sur chevron ok")
-        console.log(i);
-
-        for (let j = 0; j < divTask.length; j++) {
-            const arrow = divTask[i].firstElementChild;
-            const conteneur = divTask[i].nextElementSibling;
-
-            conteneur.classList.toggle("appear");
-            divTask[i].classList.toggle("appear");
-            console.log(conteneur);
-            arrow.classList.toggle("fa-chevron-down");
-            arrow.classList.toggle("fa-chevron-up");
-        };
-    });
-}
 
 // for (let i = 0; i < Alltasks.length; i++) {
 //     console.log(Alltasks[i]);
@@ -68,5 +31,4 @@ for (let i = 0; i < chevrons.length; i++) {
 //         removeTask();
 //     });
 // });
-
 
